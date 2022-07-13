7e7f4f9113ac56ad9cf0847fdb087721df03571b
Matthew Ihnenko <101326293+doctorInternet@users.noreply.github.com>
2022-07-13 17:03:44 +0300

    Google analytics added

--- cool-weather/cool weather/AppDelegate.swift
+++ cool-weather/cool weather/AppDelegate.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import FirebaseCore
+
+ 
+ 
+      
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +19,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // Гугл аналитика
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        // Гугл аналитика
+        
         return true
+        
+
+        
+        
+        
+        
     }
 
     // MARK: UISceneSession Lifecycle
